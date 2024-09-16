86f7055b8bd746c716b0132792d09849ebde1cc8
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2024-09-16 23:35:22 +0800

    Create TurnOffCommand.java

---
+++ TurnOffCommand.java
@@ -0,0 +1,17 @@
+package commandPattern;
+
+public class TurnOffCommand implements Command
+{
+    private Light light;
+
+    public TurnOffCommand(Light light)
+    {
+        this.light = light;
+    }
+
+    @Override
+    public void execute()
+    {
+        light.turnOff();
+    }
+}
