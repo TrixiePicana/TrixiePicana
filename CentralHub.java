58f88bd85c79c0fee9fb517984f655798089ab98
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2024-09-18 17:49:57 +0800

    centralHub.java

--- CentralHub.java
+++ CentralHub.java
@@ -1,20 +1,35 @@
 package commandPattern;
 
-public class CentralHub
+public class centralHub //Client class
 {
-    Light lightathome; Command lightOff; wallSwitch wallSwitch;
-
-    public CentralHub()
-    {
-        lightathome = new Light();
-        lightOff = new TurnOffCommand(lightathome);
-        wallSwitch = new wallSwitch();
-        wallSwitch.setCommand(lightOff);
-        wallSwitch.pressSwitch();
-    }
-
     public static void main(String[] args)
     {
-        new CentralHub();
+        Light livingRoomLight = new Light(); //Create devices.
+        Thermostat temperature = new Thermostat();
+        Videoplayer_in_PC volume = new Videoplayer_in_PC();
+
+        Command lightOn = new LightOnCommand(livingRoomLight); //Create Commands.
+        Command lightOff = new TurnOffCommand(livingRoomLight);
+        Command increaseTemperature = new IncreaseTempCommand(temperature);
+        Command decreaseTemperature = new DecreaseTempCommand(temperature);
+        Command increaseVolume = new IncreaseVolCommand(volume);
+        Command decreaseVolume = new DecreaseVolCommand(volume);
+
+        lightSwitch button = new lightSwitch(); //Issue Commands.
+        button.addCommand(lightOn);
+        button.addCommand(lightOff);
+
+        remoteControl thermostatButton = new remoteControl();
+        thermostatButton.addCommand(increaseTemperature);
+        thermostatButton.addCommand(decreaseTemperature);
+
+        Mouse mouse = new Mouse();
+        mouse.addCommand(increaseVolume);
+        mouse.addCommand(decreaseVolume);
+
+        button.executeCommands();
+        thermostatButton.executeCommands();
+        mouse.executeCommands();
     }
+
 }
